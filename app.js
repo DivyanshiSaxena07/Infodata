22a32cb904bd3045e85441d068e7b499642bbd76
DivyanshiSaxena07 <divyanshi1001@gmail.com>
2023-03-12 01:52:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+
+import user from "./route/user.router.js";
+
+app.use()
+
+app.listen(3000,()=>{
+    console.log("Server Running on this Port Number 3000");
+
+})
---
+++ route/user.router.js
@@ -0,0 +1,5 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/signin",signin);
+export default router;
